79fda1547aef92f3528bd6ea30b4a43f69a56b7d
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-21 08:30:14 -0700

    initial movie list commit

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './App.css';
 
+const App  = () => {
+  return (
+    <div className= 'App'>
+        <h1 className="movie-list-title">The Most Amazing Movie List</h1>
+    </div>
+  );
+};
+
+export default App;
