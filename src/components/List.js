00327ae16be186bbc46c3fd3498716184898562e
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-10-21 08:50:03 -0700

    added components folder

---
+++ src/components/List.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class List extends Component{
+  constructor(){
+    super()
+    
+    this.state = {
+      movies: []
+    }
+
+  }
+  render(){
+    return(
+      <div>List Component</div>
+    )
+  }
+
+}
+
+export default List
